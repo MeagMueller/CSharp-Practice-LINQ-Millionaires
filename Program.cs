f6d12c6dd5bfc0f0efb314e331eac405305a0702
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-06-20 10:56:03 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace millionaires
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
